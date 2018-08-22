9f5c454aa44c7a40b00f89100bab12eb2c3c24d9
Harshada123456 <42436284+Harshada123456@users.noreply.github.com>
2018-08-22 10:53:25 +0530

    Update sampleTest.java

--- src/test/java/sampleTest.java
+++ src/test/java/sampleTest.java
@@ -5,12 +5,18 @@ import org.junit.Test;
 public class sampleTest
 {
 	@Test
-	public void simplepass()
+	public void simpleTestpass()
 	{
 		int a=1;
 		int b=2;
 		assertTrue(a+b ==3);
 		
+	}
+	public void simpleTestFail()
+	{
+		int a=2;
+		int b=3;
+		assertFalse(a+b ==6);
 	}
 
 	
